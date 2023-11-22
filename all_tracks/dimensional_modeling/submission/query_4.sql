09e423951700f82941cdddbfad6795225907b5e3
BrunoAbbad <bruno.abbad@digifarmz.com>
2023-11-22 10:15:29 -0300

    fixing the query as requested in review

--- all_tracks/dimensional_modeling/submission/query_4.sql
+++ all_tracks/dimensional_modeling/submission/query_4.sql
@@ -16,21 +16,18 @@ streaked AS (
 SELECT 
   *,
   SUM(CASE
-    WHEN is_active <> is_active_last_year THEN 1 ELSE 0
-  END) OVER(PARTITION BY actorid ORDER BY current_year) AS streak_active,
-  SUM(CASE
-    WHEN quality_class <> quality_class_last_year THEN 1 ELSE 0
-  END) OVER(PARTITION BY actorid ORDER BY current_year) AS streak_quality_class
+    WHEN is_active <> is_active_last_year OR quality_class <> quality_class_last_year THEN 1 ELSE 0
+  END) OVER(PARTITION BY actorid ORDER BY current_year) AS streak
 FROM lagged 
 )
 
 SELECT 
   actor,
   actorid,
-  MAX(quality_class) AS quality_class,
-  MAX(is_active) AS is_active,
+  quality_class,
+  is_active,
   MIN(current_year) AS start_date,
   MAX(current_year) AS end_date,
   2003 AS current_year
 FROM streaked
-GROUP BY actor, actorid, streak_active, streak_quality_class
+GROUP BY actor, actorid, quality_class, is_active, streak
