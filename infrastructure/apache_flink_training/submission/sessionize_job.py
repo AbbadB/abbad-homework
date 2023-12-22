b3c3daacee68bded0ea8dac1f296bb19e0673598
BrunoAbbad <bruno.abbad@digifarmz.com>
2023-12-22 10:19:36 -0300

    adding IP on sessionizing and removing insert logic

--- infrastructure/apache_flink_training/submission/sessionize_job.py
+++ infrastructure/apache_flink_training/submission/sessionize_job.py
@@ -91,13 +91,13 @@ def log_aggregation():
         source_table = create_processed_events_source_kafka(t_env)
 
         aggregated_table = create_aggregated_events_sink_postgres(t_env)
-        aggregated_sink_table = create_aggregated_events_referrer_sink_postgres(t_env)
         t_env.from_path(source_table)\
             .window(
             Session.with_gap(lit(5).minutes).on(col("window_timestamp")).alias("s")
         ).group_by(
             col("s"),
-            col("host")
+            col("host"),
+            col("ip")
         ) \
             .select(
                     col("s").start.alias("event_hour"),
@@ -106,29 +106,6 @@ def log_aggregation():
             ) \
             .execute_insert(aggregated_table)
 
-        # Calculate total hits and sessions per host
-        total_hits_and_sessions = t_env.from_path(source_table)\
-            .window(
-                Session.with_gap(lit(5).minutes).on(col("window_timestamp")).alias("s")
-            ).group_by(
-                col("s"),
-                col("host")
-            ).select(
-                col("host"),
-                col("host").count.alias("num_hits"),
-                lit(1).alias("num_sessions")
-            )
-
-        # Calculate average hits per session
-        average_hits_per_session = total_hits_and_sessions.group_by(
-            col("host")
-        ).select(
-            col("host"),
-            (col("num_hits").sum / col("num_sessions").sum).alias("average_hits_per_session")
-        )
-
-        # Insert into the sink table
-        average_hits_per_session.execute_insert(aggregated_sink_table).wait()
 
 
     except Exception as e:
