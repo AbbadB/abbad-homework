b0d8e0bf0cdc0a1a3a9dc57dc2f1e1adbf61ec71
BrunoAbbad <bruno.abbad@digifarmz.com>
2024-01-25 11:28:05 -0300

    adding query 5

--- analytics/applying_analytical_patterns/queries/query_5.sql
+++ analytics/applying_analytical_patterns/queries/query_5.sql
@@ -1,30 +1,17 @@
-WITH
-  team_wins AS (
-    SELECT
-    COALESCE(CAST(team_id AS VARCHAR), 'overall') AS team_id,
-      COALESCE(player_name, 'overall') AS player_name,
-      COALESCE(CAST(season AS VARCHAR), 'overall') AS season,
-      COUNT(*) AS wins
-    FROM
-      bootcamp.nba_game_details
-      JOIN bootcamp.nba_games ON nba_game_details.game_id = nba_games.game_id
-    WHERE
-      nba_games.game_status_text = 'Final'
-      AND pts_home > pts_away
-    GROUP BY
-      GROUPING SETS (
-        (player_name, team_id),
-        (player_name, season),
-        (team_id),
-        ()
-      )
-  )
 SELECT
-  team_id,
-  MAX(wins) AS max_wins
+  COALESCE(CAST(team_id AS VARCHAR), 'overall') AS team_id,
+  COALESCE(player_name, 'overall') AS player_name,
+  COALESCE(CAST(season AS VARCHAR), 'overall') AS season,
+  SUM(home_team_wins) AS wins
 FROM
-  team_wins
+  bootcamp.nba_game_details
+  JOIN bootcamp.nba_games ON nba_game_details.game_id = nba_games.game_id
+  AND bootcamp.nba_games.home_team_id = nba_game_details.team_id
 GROUP BY
-  team_id
-LIMIT
-  1
+  GROUPING SETS (
+    (player_name, team_id),
+    (player_name, season),
+    (team_id),
+    ()
+  )
+ORDER BY wins DESC
